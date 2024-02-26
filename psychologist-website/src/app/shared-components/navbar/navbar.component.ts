2fa2b6634bf61e9dea01600d156b6d882ebff9f0
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-02-26 18:46:42 +0100

    navbar style

--- psychologist-website/src/app/shared-components/navbar/navbar.component.ts
+++ psychologist-website/src/app/shared-components/navbar/navbar.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  faSearch = faSearch;
 }
