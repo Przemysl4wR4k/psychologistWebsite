6badb36c2de8bfa972517ec1edf66605e2d7e489
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-02-28 23:00:48 +0100

    fancy button size used

--- psychologist-website/src/app/pages/contact/contact.component.ts
+++ psychologist-website/src/app/pages/contact/contact.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FancyButtonComponent, ButtonSize } from '../../shared/components/fancy-button/fancy-button.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FancyButtonComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
+  buttonSize = ButtonSize
 }
