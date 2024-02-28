76bf9b6d867205dd5cb8bed9723f205e04a3948c
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-02-28 20:02:00 +0100

    more variables, navbar styling, home-coponent subsection & fancy-button input

--- psychologist-website/src/app/pages/home/home.component.ts
+++ psychologist-website/src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FancyButtonComponent } from '../../shared/components/fancy-button/fancy-button.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FancyButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- psychologist-website/src/app/shared/components/fancy-button/fancy-button.component.ts
+++ psychologist-website/src/app/shared/components/fancy-button/fancy-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-fancy-button',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './fancy-button.component.scss'
 })
 export class FancyButtonComponent {
+  @Input() buttonText!: string
 
 }
