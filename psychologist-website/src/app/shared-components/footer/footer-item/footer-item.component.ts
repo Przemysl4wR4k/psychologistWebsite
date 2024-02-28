d5b5b61d6fef12c29666fc4b86091d78dcf05410
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-02-28 14:00:59 +0100

    using footer item

--- psychologist-website/src/app/shared-components/footer/footer-item/footer-item.component.ts
+++ psychologist-website/src/app/shared-components/footer/footer-item/footer-item.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'app-footer-item',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer-item.component.html',
   styleUrl: './footer-item.component.scss'
 })
 
 export class FooterItemComponent {
-  @Input() icon!: string;
+  @Input() icon!: IconDefinition;
   @Input() contactType!: string;
   @Input() contactInfo!: string;
 }
