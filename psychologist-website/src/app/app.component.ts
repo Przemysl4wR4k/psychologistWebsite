593db712a73b556a6e01da06407ff93b989a4d8e
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-03-01 19:59:17 +0100

    enabled znanylekarz widget displaying

--- psychologist-website/src/app/app.component.ts
+++ psychologist-website/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './sections/footer/footer.component';
 import { NavbarComponent } from './sections/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -14,4 +15,17 @@ import { AboutComponent } from './pages/about/about.component';
 })
 export class AppComponent {
   title = 'psychologist-website';
+  // Znany lekarz widget
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {}
+
+  ngOnInit(): void {
+    this.loadScript();
+  }
+
+  loadScript() {
+    const script = this.renderer.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//platform.docplanner.com/js/widget.js';
+    this.renderer.appendChild(this.document.head, script);
+  }
 }
