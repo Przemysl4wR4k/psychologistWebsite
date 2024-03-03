52103e6dbe5fa82bba46dcf49cf4c18eae69a860
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-03-03 14:43:53 +0100

    person card styled

--- psychologist-website/src/app/pages/about/person-card/person-card.component.ts
+++ psychologist-website/src/app/pages/about/person-card/person-card.component.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FancyButtonComponent } from '../../../shared/components/fancy-button/fancy-button.component';
 
 @Component({
   selector: 'app-person-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FancyButtonComponent],
   templateUrl: './person-card.component.html',
   styleUrl: './person-card.component.scss'
 })
 export class PersonCardComponent {
   @Input() personName!: string;
-  @Input() personTitle!: string;
+  @Input() tags!: string[];
   @Input() personDescription!: string;
+  
+  showWidget = false
 }
--- psychologist-website/src/app/sections/navbar/navbar.component.ts
+++ psychologist-website/src/app/sections/navbar/navbar.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FancyButtonComponent } from '../../shared/components/fancy-button/fancy-button.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, FontAwesomeModule, FancyButtonComponent],
+  imports: [RouterLink, FancyButtonComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-  faSearch = faSearch;
 }
