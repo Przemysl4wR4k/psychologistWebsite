04988392af2c5a0c9a454080564bdff4e049dbd3
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-03-04 10:59:28 +0100

    navbar active item

--- psychologist-website/src/app/sections/navbar/navbar.component.ts
+++ psychologist-website/src/app/sections/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { FancyButtonComponent } from '../../shared/components/fancy-button/fancy-button.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, FancyButtonComponent],
+  imports: [RouterLink, RouterLinkActive, FancyButtonComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
