338abf7cb5221240b19f280891856079b89c021e
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-03-11 17:50:51 +0100

    person page generated

---
+++ psychologist-website/src/app/pages/person-page/person-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './person-page.component.html',
+  styleUrl: './person-page.component.scss'
+})
+export class PersonPageComponent {
+
+}
