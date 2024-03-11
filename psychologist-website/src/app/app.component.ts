2a042dcf3e51f21f70b95e5d81664688e6731fbf
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-03-11 17:51:24 +0100

    routing from home to about

--- psychologist-website/src/app/app.component.ts
+++ psychologist-website/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, Inject, Renderer2 } from '@angular/core';
+import { Component, Inject, OnDestroy, Renderer2 } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { LayoutComponent } from './pages/layout/layout.component';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +11,33 @@ import { LayoutComponent } from './pages/layout/layout.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'psychologist-website';
   // Znany lekarz widget
-  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {}
+  constructor(
+    private renderer: Renderer2,
+    private router: Router,
+    @Inject(DOCUMENT) private document: Document) {}
+    private destroy$ = new Subject<void>()
 
   ngOnInit(): void {
-    this.loadScript();
+    this.loadScript()
+    this.router.events.pipe(takeUntil(this.destroy$)).subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0)
+      }
+    })
   }
 
   loadScript() {
-    const script = this.renderer.createElement('script');
-    script.type = 'text/javascript';
-    script.src = '//platform.docplanner.com/js/widget.js';
-    this.renderer.appendChild(this.document.head, script);
+    const script = this.renderer.createElement('script')
+    script.type = 'text/javascript'
+    script.src = '//platform.docplanner.com/js/widget.js'
+    this.renderer.appendChild(this.document.head, script)
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next()
+    this.destroy$.complete()
   }
 }
