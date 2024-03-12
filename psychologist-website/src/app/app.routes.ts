4ad15108e3f4d58ac3b3bceb028596fefd2c9467
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-03-12 20:43:44 +0100

    conflict solved

--- psychologist-website/src/app/app.routes.ts
+++ psychologist-website/src/app/app.routes.ts
@@ -3,12 +3,10 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { PersonPageComponent } from './pages/person-page/person-page.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
-    {path: 'about/:name', component: PersonPageComponent},
     {path: 'contact', component: ContactComponent},
     // {path: 'home', redirectTo: ''} //ommited on purpose
     {path: '**', component: PageNotFoundComponent}
