4d4f907e7dad0d9ae1cd97ebaafd2ee59ded55fe
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-03-13 14:00:02 +0100

    home styles

--- psychologist-website/src/app/pages/home/home.component.ts
+++ psychologist-website/src/app/pages/home/home.component.ts
@@ -10,5 +10,15 @@ import { RouterLink } from '@angular/router';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  protected getSeason() {
+    const month = new Date().getMonth();
+    if (month >= 2 && month <= 4) {
+      return 'winter';
+    } else if (month >= 5 && month <= 7) {
+      return 'summer';
+    } else if (month >= 8 && month <= 10) {
+      return 'autumn';
+    } 
+    return 'winter';
+  }
 }
