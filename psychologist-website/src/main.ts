a40aa134272dab5d3ff477c9c7dde87c15f9e248
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-03-21 19:55:04 +0100

    corrected enviromental variables

--- psychologist-website/src/main.ts
+++ psychologist-website/src/main.ts
@@ -2,19 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { initializeApp } from 'firebase/app';
-import * as dotenv from 'dotenv';
+import { environment } from './environment';
 
-dotenv.config();
 
-const firebaseConfig = {
-  apiKey: process.env?.['FIREBASE_API_KEY'],
-  authDomain: process.env?.['FIREBASE_AUTH_DOMAIN'],
-  projectId: process.env?.['FIREBASE_PROJECT_ID'],
-  storageBucket: process.env?.['FIREBASE_STORAGE_BUCKET'],
-  messagingSenderId: process.env?.['FIREBASE_MESSAGING_SENDER_ID'],
-  appId: process.env?.['FIREBASE_APP_ID'],
-  measurementId: process.env?.['FIREBASE_MEASUREMENT_ID']
-}
+const firebaseConfig = environment.firebaseConfig
 
 const app = initializeApp(firebaseConfig)
 console.log(app)
