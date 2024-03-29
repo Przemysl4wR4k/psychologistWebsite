d0058a3e8577f5c78a9975dcb9472162b7c87f17
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-03-29 11:53:06 +0100

    CONNECTED FIRESTORE

--- psychologist-website/src/app/sections/footer/footer.component.ts
+++ psychologist-website/src/app/sections/footer/footer.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faLocationDot, faPhone, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { ContactType, FooterItemComponent } from './footer-item/footer-item.component';
+import { Firestore, doc, getDoc } from '@angular/fire/firestore';
+import { collection } from 'firebase/firestore';
 
 @Component({
   selector: 'app-footer',
@@ -11,9 +13,30 @@ import { ContactType, FooterItemComponent } from './footer-item/footer-item.comp
   styleUrl: './footer.component.scss'
 })
 
-export class FooterComponent {
+export class FooterComponent implements OnInit {
   faLocationDot = faLocationDot
   faPhone = faPhone
   faPaperPlane = faPaperPlane
   contactTypes = ContactType
+
+  constructor(private firestore: Firestore) {
+
+  }
+
+  ngOnInit(): void {
+      this.getData()
+  }
+  async getData(){
+    const contactDocRef = doc(this.firestore, 'collection/contact');
+    try {
+      const docSnap = await getDoc(contactDocRef);
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+      } else {
+        console.log("No such document!");
+      }
+    } catch (error) {
+      console.error("Error getting document:", error);
+    }
+  }
 }
--- psychologist-website/src/main.ts
+++ psychologist-website/src/main.ts
@@ -1,15 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { initializeApp } from 'firebase/app';
-import { environment } from './environment';
-
-
-const firebaseConfig = environment.firebaseConfig
-
-const app = initializeApp(firebaseConfig)
-console.log(app)
-// console.log(appFB)
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
