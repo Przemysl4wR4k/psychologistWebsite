dfce800d594251ee97fa42c7f63bf72582c636b2
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-04-22 19:40:19 +0200

    cookies policy page

--- psychologist-website/src/app/app.routes.ts
+++ psychologist-website/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { CookiePolicyPageComponent } from './pages/cookie-policy-page/cookie-policy-page.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
     {path: 'contact', component: ContactComponent},
     // {path: 'home', redirectTo: ''} //ommited on purpose
+    {path: 'cookies-policy', component: CookiePolicyPageComponent},
     {path: '**', component: PageNotFoundComponent}
 ]
